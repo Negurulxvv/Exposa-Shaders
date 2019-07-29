0691f6023b5d959086db7c7834cff89519ad3092
Negurulxvv <53142647+Negurulxvv@users.noreply.github.com>
2019-07-29 13:13:39 +0300

    Updated Shadow Details
    Now the shadows look more detailed, but the change the shadow map resolution, go into framebuffer.glsl in the lib folder and change It's value.

--- shaders/lib/framebuffer.glsl
+++ shaders/lib/framebuffer.glsl
@@ -1,7 +1,7 @@
 const int RGBA16                 = 1;             
 const int gcolorFormat           =  RGBA16;
-const float shadowMapResolution    = 4096; //[2048 4096 8192]
-const int noiseTextureResolution = 512;
+const int shadowMapResolution    = 8192; //Resolution of the Shadows
+const int noiseTextureResolution = 256; //Resolution of the noise
 
 const float sunPathRotation   = -40.0;
 
@@ -13,6 +13,19 @@ uniform sampler2D gdepth;
 #define GDEPTH_OUT      gl_FragData[1]
 #define GNORMAL_OUT     gl_FragData[2]
 
+#define TORCH_INSENITY 7.0
+
+vec3 getAlbedo(in vec2 coord) {
+    return texture2D(gcolor, coord).rgb;
+}
+
+vec3 getNormal(in vec2 coord) {
+    return texture2D(gnormal, coord).rgb * 2.0 - 1.0;
+}
+
+float getEmission(in vec2 coord) {
+    return texture2D(gdepth, coord).a;
+}
 
 float getTorchLightStrength(in vec2 coord) {
     return texture2D(gdepth, coord).r;
